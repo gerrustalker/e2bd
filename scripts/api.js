62bccd244990c7690a97e23afd0ef59b2bcbed39
Andrey Stalker <gerrustalker@users.noreply.github.com>
2021-01-17 01:17:19 +0300

    working on api
    added some packages

---
+++ scripts/api.js
@@ -0,0 +1,65 @@
+const simpleGit = require('simple-git');
+const fs = require('fs')
+const express = require('express')
+
+const git = simpleGit();
+const app = express();
+const jsonParser = bodyParser.json();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/data/grab", function(req, res){
+      
+    var content = fs.readFileSync("/data/grab/grabbed.json", "utf8");
+    var chips = JSON.parse(content);
+    res.send(chips);
+});
+
+app.get("/data/grab/:id", function(req, res){
+      
+    var id = req.params.id;
+    var content = fs.readFileSync("/data/grab/grabbed.json", "utf8");
+    var chips = JSON.parse(content);
+    var chip = null;
+    
+    for(var i = 0; i < users.length; i++) {
+        if(users[i].id == id){
+            chip = chips[i];
+            break;
+        }
+    }
+    // отправляем пользователя
+    if(chip){
+        res.send(chip);
+    }
+    else{
+        res.status(404).send();
+    }
+});
+// получение отправленных данных
+app.post("/data/grab", jsonParser, function (req, res) {
+     
+    if(!req.body) return res.sendStatus(400);
+     
+    var chipName = req.body.name;
+    var chipFilename = req.body.filename;
+    var chipOwner = req.body.owner;
+    var chip = {name: chipName, filename: chipFilename, owner: chipOwner};
+     
+    var data = fs.readFileSync("/data/grab/grabbed.json", "utf8");
+    var chops = JSON.parse(data);
+     
+    var id = Math.max.apply(Math, chips.map(function(o) {return o.id;}))
+    chip.id = id+1;
+    chip.push(chip);
+
+    var data = JSON.stringify(users);
+    fs.writeFileSync("/data/grab/grabbed.json", data);
+    res.send(chip);
+
+    console.log('Sending ', chipFilename)
+});
+
+app.listen(3000, function(){
+    console.log("Server waiting for connecting...");
+});
---
+++ scripts/console.js
@@ -0,0 +1,112 @@
+const simpleGit = require('simple-git');
+const readline = require('readline')
+const fs = require('fs')
+const express = require('express')
+const git = simpleGit();
+const remote = `https://gerrustalker:DCP_Foundation!1@github.com/gerrustalker/e2bd`;
+var started = 0
+
+git.init().addRemote('origin', '${remote}', started = 1);
+
+const name = 'Andrey Stalker';
+
+// console.warn(`Test warn, ${name}!`);
+
+/*function onInit (err, initResult) {
+    console.log(initResult);
+    // setTimeout(started = 1, 1000)
+}\*
+
+
+/*function onRemoteAdd (err, addRemoteResult) {
+    console.error(err)
+}*/
+
+/*git.silent(true)
+    .clone(remote)
+    .then(() => console.log('Cloned repo'))
+    .catch((err) => console.error('Error ocured: ', err));*/
+
+/*function askQuestion(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
+}*/
+
+if(started == 1) {
+    console.log("\x1b[32m", 'Node has been started')
+
+    git.checkIsRepo()
+        //.then(() => console.log('You are in repo.'))
+        testCommit();
+
+    function testCommit() {
+        git.add('file.txt');
+        git.commit("gitapi.js says hi!");
+        git.pull(remote, 'origin', 'main');
+        git.fetch(remote, 'origin')
+        console.log('Test commit initiated at ', remote, '\x1b[37m')
+    }
+
+    consoleInput()
+
+    function consoleInput() {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        rl.question('> ', (answer) => {
+            //console.log(`Getted command: ${answer}`);
+    
+            /*if(answer == 'exit') {
+                console.log('Shutting down...')
+                process.exit(1)
+            }
+            
+            if(answer == 'reboot') {
+                console.log('Rebooting...')
+                exec("npm restart")
+                consoleInput()
+            }
+    
+            if(answer.substring(6) == 'commit') {
+                console.log('Trying to commit with string ')
+                consoleInput()
+            }*/
+        
+            switch(answer) {
+                case('exit'):
+                    console.log('Shutting down...')
+                    process.exit(1)
+                break
+
+                case(answer.substring(6) == 'reboot'):
+                    console.log('Rebooting...')
+                    exec("npm restart")
+                    consoleInput()
+                break
+
+                case('commit'):
+                    console.log('Trying to commit with string ')
+                    consoleInput()
+                break
+
+                default:
+                    console.log('Command not found!')
+                    consoleInput()
+                break
+            }
+            
+            //consoleInput()
+            rl.close();
+        });
+    }
+}
+
