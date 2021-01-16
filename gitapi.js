0af548c1014f12ace2b398a6f3e6eace9dee1cda
Andrey Stalker <gerrustalker@users.noreply.github.com>
2021-01-16 19:07:55 +0300

    readline added + some code changes

--- gitapi.js
+++ gitapi.js
@@ -1,34 +1,67 @@
 const simpleGit = require('simple-git');
+const readline = require('readline')
 const git = simpleGit();
 const remote = `https://gerrustalker:DCP_Foundation!1@github.com/gerrustalker/e2bd`;
 
-git.init(onInit).addRemote('origin', '${remote}', onRemoteAdd);
+git.init(onInit).addRemote('origin', '${remote}', started = 1);
 
 const name = 'Andrey Stalker';
 
 // console.warn(`Test warn, ${name}!`);
 
 function onInit (err, initResult) {
-	console.log(initResult)
+    console.log(initResult)
+    // started = 1
 }
-function onRemoteAdd (err, addRemoteResult) {
+/*function onRemoteAdd (err, addRemoteResult) {
     console.error(err)
-}
+    
+}*/
 
 /*git.silent(true)
     .clone(remote)
     .then(() => console.log('Cloned repo'))
     .catch((err) => console.error('Error ocured: ', err));*/
 
-git.checkIsRepo()
+function askQuestion(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
+}
+
+if(started == 1) {
+    git.checkIsRepo()
     .then(() => console.log('You are in repo.'));
     testCommit()
 
-function testCommit() {
-    git.add('file.txt');
-    git.commit("js commit test");
-    git.pull(remote, 'origin', 'main');
-    git.fetch(remote, 'main')
-    console.log('Test commit initiated')
+    function testCommit() {
+        git.add('file.txt');
+        git.commit("gitapi.js says hi!");
+        git.pull(remote, 'origin', 'main');
+        git.fetch(remote, 'origin')
+        console.log('Test commit initiated')
+    }
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Command: ', (answer) => {
+        //console.log(`Getted command: ${answer}`);
+
+        if(answer == 'exit') {
+            console.log('Shutting down...')
+            process.exit(1)
+        }
+    
+        rl.close();
+    });
 }
 
