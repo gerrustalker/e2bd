4b04b8e2aa2cf6130272267cce4a7a107e81458b
Andrey Stalker <gerrustalker@users.noreply.github.com>
2021-01-16 20:58:22 +0300

    uh
    6-7h for 112 lines B\

--- gitapi.js
+++ gitapi.js
@@ -2,20 +2,22 @@ const simpleGit = require('simple-git');
 const readline = require('readline')
 const git = simpleGit();
 const remote = `https://gerrustalker:DCP_Foundation!1@github.com/gerrustalker/e2bd`;
+var started = 0
 
-git.init(onInit).addRemote('origin', '${remote}', started = 1);
+git.init().addRemote('origin', '${remote}', started = 1);
 
 const name = 'Andrey Stalker';
 
 // console.warn(`Test warn, ${name}!`);
 
-function onInit (err, initResult) {
-    console.log(initResult)
-    // started = 1
-}
+/*function onInit (err, initResult) {
+    console.log(initResult);
+    // setTimeout(started = 1, 1000)
+}\*
+
+
 /*function onRemoteAdd (err, addRemoteResult) {
     console.error(err)
-    
 }*/
 
 /*git.silent(true)
@@ -23,7 +25,7 @@ function onInit (err, initResult) {
     .then(() => console.log('Cloned repo'))
     .catch((err) => console.error('Error ocured: ', err));*/
 
-function askQuestion(query) {
+/*function askQuestion(query) {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -33,35 +35,80 @@ function askQuestion(query) {
         rl.close();
         resolve(ans);
     }))
-}
+}*/
 
 if(started == 1) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    console.log('Node has been started')
+
     git.checkIsRepo()
-    .then(() => console.log('You are in repo.'));
-    testCommit()
+        //.then(() => console.log('You are in repo.'))
+        testCommit();
 
     function testCommit() {
         git.add('file.txt');
         git.commit("gitapi.js says hi!");
         git.pull(remote, 'origin', 'main');
         git.fetch(remote, 'origin')
-        console.log('Test commit initiated')
+        console.log('Test commit initiated at ', remote)
     }
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+    consoleInput()
 
-    rl.question('Command: ', (answer) => {
-        //console.log(`Getted command: ${answer}`);
+    function consoleInput() {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-        if(answer == 'exit') {
-            console.log('Shutting down...')
-            process.exit(1)
-        }
+        initConsole()
+    }
+
+    function initConsole() {
+        rl.question('> ', (answer) => {
+            //console.log(`Getted command: ${answer}`);
     
-        rl.close();
-    });
+            /*if(answer == 'exit') {
+                console.log('Shutting down...')
+                process.exit(1)
+            }
+            
+            if(answer == 'reboot') {
+                console.log('Rebooting...')
+                exec("npm restart")
+                consoleInput()
+            }
+    
+            if(answer.substring(6) == 'commit') {
+                console.log('Trying to commit with string ')
+                consoleInput()
+            }*/
+        
+            switch(answer) {
+                case('exit'):
+                    console.log('Shutting down...')
+                    process.exit(1)
+                break
+
+                case('reboot'):
+                    console.log('Rebooting...')
+                    exec("npm restart")
+                    consoleInput()
+                break
+
+                case('commit'):
+                    console.log('Trying to commit with string ')
+                    consoleInput()
+                break
+            }
+            
+            //consoleInput()
+            rl.close();
+        });
+    }
 }
 
